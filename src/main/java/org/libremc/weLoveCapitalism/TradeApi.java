08cce91c71ea14913882671175ffcebc5b6ff719
Francesco Serangeli <serangelicloud.com@gmail.com>
2025-09-20 00:07:19 +0200

    working standalone

--- src/main/java/org/libremc/weLoveCapitalism/TradeApi.java
+++ src/main/java/org/libremc/weLoveCapitalism/TradeApi.java
@@ -3,10 +3,12 @@ package org.libremc.weLoveCapitalism;
 import static spark.Spark.*;
 import com.google.gson.Gson;
 import java.sql.SQLException;
+import java.io.IOException;
 
 public class TradeApi {
 
     private static final Gson gson = new Gson();
+    private static TradesDatabaseStandalone db;
 
     public static void start() {
         // Use port 8080 (can change if needed)
@@ -16,8 +18,8 @@ public class TradeApi {
             res.type("application/json");
 
             try {
-                // Use your existing TradesDatabase to get trades
-                return gson.toJson(TradesDatabase.parseTrades());
+                // Fetch trades from the standalone database
+                return gson.toJson(db.parseTrades());
             } catch (SQLException e) {
                 e.printStackTrace();
                 res.status(500);
@@ -26,5 +28,22 @@ public class TradeApi {
         });
 
         System.out.println("[WLC] Trade API running on port 8080");
+    }
+
+    // --- Standalone entry point ---
+    public static void main(String[] args) {
+        System.out.println("[WLC] Starting Trade API in standalone mode...");
+
+        try {
+            // Use the same DB file your plugin uses
+            db = new TradesDatabaseStandalone("trade_history.db");
+        } catch (SQLException | IOException e) {
+            e.printStackTrace();
+            System.err.println("[WLC] Failed to open database. Exiting.");
+            System.exit(1);
+        }
+
+        // Start the API
+        start();
     }
 }
